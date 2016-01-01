b3189da9ba6a2c2b9293ceea08f17b036e278aa6
James Andrews <james@openbridge.com>
2016-01-01 22:04:17 +0900

    final fixes

--- buildPDF.php
+++ buildPDF.php
@@ -15,25 +15,31 @@ foreach($addresses as $address)
 
     $pdf->SetFont('Arial', "", 8);
     $pdf->setXY(30, 3);
-    $pdf->Cell(40, 10, "The Andrews Family");
+    $pdf->Cell(45, 10, "The Andrews Family");
     $pdf->setXY(30, 6);
-    $pdf->Cell(40, 10, "Sakaine 5-7-29");
+    $pdf->Cell(45, 10, "Sakaine 5-7-29");
     $pdf->setXY(30, 9);
-    $pdf->Cell(40,10, "Kashiwa-shi Chiba-ken Japan");
+    $pdf->Cell(45,10, "Kashiwa-shi Chiba-ken Japan");
     $pdf->setXY(30, 12);
-    $pdf->Cell(40, 10, "277-0053");
+    $pdf->Cell(45, 10, "277-0053");
 
 
+    $startPosition = 45;
     $pdf->SetFont('Arial', "", 12);
-    $pdf->setXY(30,50);
-    $pdf->Cell(40, 10, $address[0]);
+    $pdf->setXY(30,$startPosition);
+    $pdf->Cell(45, 10, trim($address[0]));
     $pdf->SetFont('Arial', "", 10);
-    $pdf->setXY(30, 55);
-    $pdf->Cell(40, 10, $address[1]);
-    $pdf->setXY(30, 60);
-    $pdf->Cell(40,10, $address[2].', '. $address[3] . ' ' . $address[4]);
-    $pdf->setXY(30, 65);
-    $pdf->Cell(40, 10, $address[5]);
+    if(isset($address[6]) && $address[6] != "")
+    {
+        $pdf->setXY(30, $startPosition+=5);
+        $pdf->Cell(45, 10, trim($address[6]));
+    }
+    $pdf->setXY(30, $startPosition+=5);
+    $pdf->Cell(45, 10, trim($address[1]));
+    $pdf->setXY(30, $startPosition+=5);
+    $pdf->Cell(45,10, trim($address[2]) . ', '. trim($address[3]) . ' ' . trim($address[4]));
+    $pdf->setXY(30, $startPosition+=5);
+    $pdf->Cell(45, 10, trim($address[5]));
 }
 
 $pdf->Output();
